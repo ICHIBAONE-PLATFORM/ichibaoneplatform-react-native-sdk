5c73623bff3b6ea85b41e1b08edcdf84647c7e1a
huyvv <huyvv@ichiba.vn>
2025-04-21 14:12:26 +0700

    feat: Test image notification firebase

---
+++ example/ios/NotificationImage/NotificationService.swift
@@ -0,0 +1,31 @@
+//
+//  NotificationService.swift
+//  NotificationImage
+//
+//  Created by Van Huy on 21/4/25.
+//
+
+import UserNotifications
+import ichibaoneplatform_ios_sdk
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        Utils.notificationHelper(request: request, contentHandler: contentHandler)
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
